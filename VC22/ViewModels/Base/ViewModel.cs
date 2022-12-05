85dea24023432c824d11ef0f0ec5b22d53c50085
NSIST96 <amelin.good@mail.ru>
2022-12-05 18:29:58 +0300

    Тестирование работы модели-представления

--- VC22/ViewModels/Base/ViewModel.cs
+++ VC22/ViewModels/Base/ViewModel.cs
@@ -1,14 +1,36 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace VC22.ViewModels.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string PropertyName)
+        protected virtual void OnPropertyChanged([CallerMemberName] string? PropertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        private bool _Disposed;
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
         }
     }
 }
--- VC22/ViewModels/MainWindowViewModel.cs
+++ VC22/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VC22.ViewModels.Base;
 
 namespace VC22.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Analis";
+
+        /// <summary> Заголовок окна </summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title= value;
+            //    //OnPropertyChanged();
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        } 
+        #endregion
     }
 }
