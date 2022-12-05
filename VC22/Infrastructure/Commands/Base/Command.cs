3dce5f2c1bd7de95a56cc915a37b0f7320431939
NSIST96 <amelin.good@mail.ru>
2022-12-05 20:50:04 +0300

    Абстрактный класс команд

--- VC22/Infrastructure/Commands/Base/Command.cs
+++ VC22/Infrastructure/Commands/Base/Command.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace VC22.Infrastructure.Commands.Base
 {
-    internal class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value; 
+            remove => CommandManager.RequerySuggested-= value;
+        }
+
+        public abstract bool CanExecute(object parameter); //{ throw new NotImplementedException(); }
+
+        /// <summary> Логика команды </summary>
+        /// <param name="parameter"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public abstract void Execute(object parameter); //{ throw new NotImplementedException(); }
     }
 }
