8660d64f1b87c35c98fe9ba9f967764a8c16ab52
NSIST96 <amelin.good@mail.ru>
2022-12-05 17:25:34 +0300

    Добавлен базовый класс модели представления

---
+++ VC22/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace VC22.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
