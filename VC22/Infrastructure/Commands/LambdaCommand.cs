0ae8c5998765bf5e16fa6249d2dab40dceb5839a
NSIST96 <amelin.good@mail.ru>
2022-12-06 09:34:07 +0300

    Главный класс комманды

---
+++ VC22/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using VC22.Infrastructure.Commands.Base;
+
+namespace VC22.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        //Поля readonly немного ускоряют работу приложения
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+        public LambdaCommand( Action<object> Execute, Func<object, bool> CanExecute = null) 
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Exception)); // делегат
+            _CanExecute = CanExecute; // делегат
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
