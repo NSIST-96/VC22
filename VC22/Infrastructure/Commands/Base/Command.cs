c8bf78b3ef159276393f1d8424efd8019b8c5a67
NSIST96 <amelin.good@mail.ru>
2022-12-06 12:56:41 +0300

    Добавляем в модель-представления комманду выхода из приложения

--- VC22/Infrastructure/Commands/Base/Command.cs
+++ VC22/Infrastructure/Commands/Base/Command.cs
@@ -8,7 +8,7 @@ namespace VC22.Infrastructure.Commands.Base
         public event EventHandler? CanExecuteChanged
         {
             add => CommandManager.RequerySuggested += value; 
-            remove => CommandManager.RequerySuggested-= value;
+            remove => CommandManager.RequerySuggested -= value;
         }
 
         public abstract bool CanExecute(object parameter); //{ throw new NotImplementedException(); }
